368e01cc28e491b529c6d5d504e008081c66e607
Peter <62403514+PeterAyad@users.noreply.github.com>
2021-12-22 13:41:01 +0200

    Add files via upload

--- ALUwithFlags/ALU.vhd
+++ ALUwithFlags/ALU.vhd
@@ -26,6 +26,19 @@ ARCHITECTURE arch_ALU OF ALU IS
     SIGNAL inc : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL Nop2 : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL adder_carry, subtractor_carry, inc_carry, zero : STD_LOGIC;
+
+    CONSTANT SETC_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0000";
+    CONSTANT NOT_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0001";
+    CONSTANT INC_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0010";
+    CONSTANT PASSOP1_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0011";
+    CONSTANT ADD_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0100";
+    CONSTANT SUB_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0101";
+    CONSTANT AND_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0110";
+    CONSTANT PASSOP2_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "0111";
+    CONSTANT CLEARZ_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "1000";
+    CONSTANT CLEARN_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "1001";
+    CONSTANT CLEARC_c : STD_LOGIC_VECTOR (3 DOWNTO 0) := "1010";
+
 BEGIN
     u1 : my_nadder GENERIC MAP(16) PORT MAP(op1, op2, '0', adder, adder_carry);
     -- A - B = A + ~B + 1
@@ -37,40 +50,39 @@ BEGIN
     Nop2 <= NOT(op2);
     zero <= '1' WHEN outp = X"0000" ELSE
         '0';
-
-    outp <= op1 WHEN ctrl = "0000" -- Pass op1
+    outp <= op1 WHEN ctrl = PASSOP1_c -- Pass op1
         ELSE
-        op2 WHEN ctrl = "0001" -- Pass op2
+        op2 WHEN ctrl = PASSOP2_c -- Pass op2
         ELSE
-        NOT(op1) WHEN ctrl = "0011" -- NOT
+        NOT(op1) WHEN ctrl = NOT_c -- NOT
         ELSE
-        inc WHEN ctrl = "0100" -- INC
+        inc WHEN ctrl = INC_c -- INC
         ELSE
-        adder WHEN ctrl = "0101" -- ADD
+        adder WHEN ctrl = ADD_c -- ADD
         ELSE
-        subtractor WHEN ctrl = "0110" -- SUB
+        subtractor WHEN ctrl = SUB_c -- SUB
         ELSE
-        op1 AND op2 WHEN ctrl = "0111" -- AND
+        op1 AND op2 WHEN ctrl = AND_c -- AND
         ELSE
         (OTHERS => '0');
 
-    new_flags <= current_flags(2 DOWNTO 1) & '1' WHEN ctrl = "0010" -- SETC
+    new_flags <= current_flags(2 DOWNTO 1) & '1' WHEN ctrl = SETC_c -- SETC
         ELSE
-        zero & outp(15) & current_flags(0) WHEN ctrl = "0011" -- NOT
+        zero & outp(15) & current_flags(0) WHEN ctrl = NOT_c -- NOT
         ELSE
-        zero & outp(15) & current_flags(0) WHEN ctrl = "0100" -- INC
+        zero & outp(15) & current_flags(0) WHEN ctrl = INC_c -- INC
         ELSE
-        zero & outp(15) & adder_carry WHEN ctrl = "0101" -- ADD 
+        zero & outp(15) & adder_carry WHEN ctrl = ADD_c -- ADD 
         ELSE
-        zero & outp(15) & NOT(subtractor_carry) WHEN ctrl = "0110" -- SUB
+        zero & outp(15) & NOT(subtractor_carry) WHEN ctrl = SUB_c -- SUB
         ELSE
-        zero & outp(15) & current_flags(0) WHEN ctrl = "0111" -- AND
+        zero & outp(15) & current_flags(0) WHEN ctrl = AND_c -- AND
         ELSE
-        '0' & current_flags(1 DOWNTO 0) WHEN ctrl = "1000" -- CLEAR Z
+        '0' & current_flags(1 DOWNTO 0) WHEN ctrl = CLEARZ_c -- CLEAR Z
         ELSE
-        current_flags(2) & '0' & current_flags(0) WHEN ctrl = "1001" -- CLEAR N
+        current_flags(2) & '0' & current_flags(0) WHEN ctrl = CLEARN_c -- CLEAR N
         ELSE
-        current_flags(2 DOWNTO 1) & '0' WHEN ctrl = "1010" -- CLEAR C 
+        current_flags(2 DOWNTO 1) & '0' WHEN ctrl = CLEARC_c -- CLEAR C 
         ELSE
         current_flags;
 
