85cff7d3a979072821c2d30914e4f54b221a033b
osamamagdy <osamamagdy174@gmail.com>
2021-12-22 07:35:27 +0200

    modify decode stage

--- DecodeStage/Decode_stage.vhd
+++ DecodeStage/Decode_stage.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE work.ALL;
 ----------------------------------------------------------------------------------------------------
 ---------------------------------4x1 MUX---------------------------------------------------------------------
 ENTITY mux4 IS
@@ -20,8 +21,11 @@ BEGIN
 END ARCHITECTURE;
 ----------------------------------------------------------------------------------------------------
 ---------------------------------2x1 MUX---------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE work.ALL;
 ENTITY mux2 IS
-    GENERIC (n : integer := 16)
+    GENERIC (n : integer := 16);
     PORT (
         in0, in1 : IN STD_LOGIC_VECTOR (n-1 DOWNTO 0);
         sel : IN STD_LOGIC;
@@ -38,6 +42,9 @@ END ARCHITECTURE;
 
 -------------------------------------------------------------------------------------------------------------
 ----------------------------------------------Whole Decode Stage---------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE work.ALL;
 ENTITY myDecode IS
     PORT (
 
@@ -122,7 +129,7 @@ myReg_file : entity work.reg_file port map(
 myCU : entity work.CU port map(
     RET => IN_RET,
     IS_EXCEPTION => IS_EXCEPTION ,
-    RTI => IN_RIT,
+    RTI => IN_RTI,
     Flag_reg => Flag_Reg,
     OP_CODE => INSTRUCTION(31 DOWNTO 27),
     EX_Flush => OUT_EX_FLUSH,
@@ -132,71 +139,71 @@ myCU : entity work.CU port map(
     HLT => HLT,
     MUX_5 => mux_5_selector,
     MUX_1 =>OUT_MUX_1 ,
-    Control_Signals =>CONTROL_SIGNALS;
+    Control_Signals =>CONTROL_SIGNALS
 );
 
-OR_gate_out <= OR(OR_gate_in , IS_HAZARD);
+OR_gate_out <= (OR_gate_in OR IS_HAZARD);
 
 myMux4 : entity work.mux2 GENERIC MAP (17) port map(
-    sel => OR_gate_out;
-    in0 =>CONTROL_SIGNALS;
-    in1 => (OTHERS => '0');
-    out1 => Buffer_Input;
+    sel => OR_gate_out,
+    in0 =>CONTROL_SIGNALS,
+    in1 => (OTHERS => '0'),
+    out1 => Buffer_Input
 );
 
 myMux10 : entity work.mux2 GENERIC MAP (16) port map(
-    sel => OR_gate_out;
-    in0 =>reg_file_out3;
-    in1 => INSTRUCTION(15 DOWNTO 0);
-    out1 => second_operand;
+    sel => OR_gate_out,
+    in0 =>reg_file_out3,
+    in1 => INSTRUCTION(15 DOWNTO 0),
+    out1 => second_operand
 );
 
 myMux5 : entity work.mux4 port map(
-    in0 =>IN_PORT; 
-    in1 => reg_file_out1 ; 
-    in2 => reg_file_out2; 
-    in3 => (OTHERS => '0');
-    sel => mux_5_selector;
-    out1 => RS_DATA;
+    in0 =>IN_PORT,
+    in1 => reg_file_out1 , 
+    in2 => reg_file_out2,
+    in3 => (OTHERS => '0'),
+    sel => mux_5_selector,
+    out1 => RS_DATA
 
 );
 
 
 myBuffer : entity work.ID_Buffer port map (
     
-    Clk => Clk; 
-    rst => OR_gate_out;
+    Clk => Clk, 
+    rst => OR_gate_out,
 
     --------Inputs
-    Control_Signal => CONTROL_SIGNALS;
-    IN_PC => PC;
-    IN_NEXT_PC => Next_PC;
-    IN_RS_DATA => RS_DATA;
-    IN_SEC_OP => second_operand;
-    IN_RD_ADD => INSTRUCTION(26 DOWNTO 24 );
-    IN_RS_ADD => INSTRUCTION( 23 DOWNTO 21);
-    IN_RT_ADD => INSTRUCTION( 20 DOWNTO 18);
+    Control_Signal => CONTROL_SIGNALS,
+    IN_PC => PC,
+    IN_NEXT_PC => Next_PC,
+    IN_RS_DATA => RS_DATA,
+    IN_SEC_OP => second_operand,
+    IN_RD_ADD => INSTRUCTION(26 DOWNTO 24 ),
+    IN_RS_ADD => INSTRUCTION( 23 DOWNTO 21),
+    IN_RT_ADD => INSTRUCTION( 20 DOWNTO 18),
 
     -------Outputs
-    OUT_PC       =>OUT_PC      ;      
-    OUT_NEXT_PC  =>OUT_NEXT_PC ;      
-    OUT_RS_DATA  =>OUT_RS_DATA ;        
-    OUT_SEC_OP   =>OUT_SEC_OP  ;        
-    OUT_RD_ADD   =>OUT_RD_ADD  ;       
-    OUT_RS_ADD   =>OUT_RS_ADD  ;     
-    OUT_RT_ADD   =>OUT_RT_ADD  ;    
+    OUT_PC       =>OUT_PC      ,      
+    OUT_NEXT_PC  =>OUT_NEXT_PC ,      
+    OUT_RS_DATA  =>OUT_RS_DATA ,        
+    OUT_SEC_OP   =>OUT_SEC_OP  ,        
+    OUT_RD_ADD   =>OUT_RD_ADD  ,       
+    OUT_RS_ADD   =>OUT_RS_ADD  ,     
+    OUT_RT_ADD   =>OUT_RT_ADD  ,    
 
     -------Outputs from Control register
-    MEM_VAL      => MEM_VAL    ;          
-    MEM_ADD      => MEM_ADD    ;           
-    SP_OP        => SP_OP      ;     
-    SP_NUM       => SP_NUM     ;        
-    WB           => WB         ;   
-    MEM          => MEM        ;      
-    EX           => EX         ;       
-    RTI          => RTI        ;     
-    BACKUP_FLAG  => BACKUP_FLAG;         
-    RET          => RET        ;        
+    MEM_VAL      => MEM_VAL    ,          
+    MEM_ADD      => MEM_ADD    ,           
+    SP_OP        => SP_OP      ,     
+    SP_NUM       => SP_NUM     ,        
+    WB           => WB         ,   
+    MEM          => MEM        ,      
+    EX           => EX         ,       
+    RTI          => RTI        ,     
+    BACKUP_FLAG  => BACKUP_FLAG,         
+    RET          => RET                 
 );
 
 
