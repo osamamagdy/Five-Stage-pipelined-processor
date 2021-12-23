f3d619424c01aac10c87dfe5460fd837fae0b307
osamamagdy <osamamagdy174@gmail.com>
2021-12-24 01:49:03 +0200

    merge1

--- Fetch_Decode.vhd
+++ Fetch_Decode.vhd
@@ -4,18 +4,32 @@ USE ieee.numeric_std.ALL;
 USE work.ALL;
 ENTITY Fetch_Decode IS
     PORT (
+
         reset : IN STD_LOGIC;
+        clk : IN STD_LOGIC;
+        ----------------Decode inputs from outside -----------------
+        IS_EXCEPTION, IN_RET, IN_RTI, IS_HAZARD : IN STD_LOGIC;
+
         IN_PORT : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    
+        Write_Address : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+    
+        Write_Value : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+    
+        Write_EN : IN STD_LOGIC;
+    
+        Flag_Reg : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+    
+        -------------------Fetch inputs from outside-----------------------------
 
         JumpAddress : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
         Exception : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
         Stack : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        clk : IN STD_LOGIC;
-        MUX1_SEL : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
         HLT : IN STD_LOGIC;
         --
         MO_1 : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
         --
+        ------------------------------------------
         OUT_PC : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
         OUT_NEXT_PC : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
         OUT_RS_DATA : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
@@ -26,7 +40,6 @@ ENTITY Fetch_Decode IS
         OUT_EX_FLUSH : OUT STD_LOGIC;
         OUT_IF_FLUSH : OUT STD_LOGIC;
         HLT : OUT STD_LOGIC;
-        OUT_MUX_1 : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
 
         -------Outputs from Control register
         MEM_VAL : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
@@ -44,6 +57,7 @@ ENTITY Fetch_Decode IS
 END Fetch_Decode;
 
 ARCHITECTURE my_FD OF Fetch_Decode IS
+    SIGNAL MUX1_SEL : STD_LOGIC_VECTOR(1 DOWNTO 0);
     SIGNAL pcOut : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL next_pcout : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL IR : STD_LOGIC_VECTOR(31 DOWNTO 0)
@@ -54,6 +68,30 @@ BEGIN
         );
 
     mydecode : ENTITY work.myDecode PORT MAP (
+
+        OUT_PC       => OUT_PC        ,   
+        OUT_NEXT_PC  => OUT_NEXT_PC   ,  
+        OUT_RS_DATA  => OUT_RS_DATA   ,  
+        OUT_SEC_OP   => OUT_SEC_OP    , 
+        OUT_RD_ADD   => OUT_RD_ADD    , 
+        OUT_RS_ADD   => OUT_RS_ADD    ,    
+        OUT_RT_ADD   => OUT_RT_ADD    ,  
+        OUT_EX_FLUSH => OUT_EX_FLUSH  ,   
+        OUT_IF_FLUSH => OUT_IF_FLUSH  ,   
+        HLT          => HLT           , 
+        OUT_MUX_1    => MUX1_SEL      , 
+
+        -------Outputfrom Control register
+        MEM_VAL      => MEM_VAL       ,   
+        MEM_ADD      => MEM_ADD       ,   
+        SP_OP        => SP_OP         ,  
+        SP_NUM       => SP_NUM        ,         
+        WB           => WB            ,    
+        MEM          => MEM           ,      
+        EX           => EX            ,     
+        RTI          => RTI           ,         
+        BACKUP_FLAG  => BACKUP_FLAG   ,         
+        RET          => RET                  
         );
 
 END ARCHITECTURE;
