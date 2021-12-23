8c562854e2603ab53c24321bc81f3847e3d96600
Ahmed Mohamed Abdelhamed <ahmedmohamedcr60@gmail.com>
2021-12-24 01:55:20 +0200

    update

--- Fetch/IF-ID_BUFFER.vhd
+++ Fetch/IF-ID_BUFFER.vhd
@@ -4,14 +4,14 @@ USE ieee.std_logic_1164.ALL;
 ENTITY IFID_BUFFER IS
     PORT (
         PC_IN, PC_NEXT_IN, IR_IN    : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-        CLK, RST, STALL, FLUSH      : IN STD_LOGIC;
+        CLK, RST, FLUSH      : IN STD_LOGIC;
         PC_OUT, PC_NEXT_OUT, IR_OUT : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE StructIFID_BUFFER OF IFID_BUFFER IS
 BEGIN
-    PROCESS (CLK, RST, stall)
+    PROCESS (CLK, RST)
     BEGIN
         IF (rising_edge(CLK)) THEN
 
@@ -20,12 +20,11 @@ BEGIN
                 PC_NEXT_OUT <= (OTHERS => '0');
                 IR_OUT      <= (OTHERS => '0');
 
-            ELSE IF (STALL = '0') THEN
+            ELSE 
                 PC_OUT      <= PC_IN;
                 PC_NEXT_OUT <= PC_NEXT_IN;
                 IR_OUT      <= IR_IN;
             END IF;
-        END IF;
     END IF;
 END PROCESS;
 END StructIFID_BUFFER;
