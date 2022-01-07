bcbdf46744456ea2f91a26532f147f42de4f516d
Ahmed Mohamed Abdelhamed <ahmedmohamedcr60@gmail.com>
2022-01-07 22:28:40 +0200

    reset fetch

--- Fetch/PC_REG.vhd
+++ Fetch/PC_REG.vhd
@@ -5,18 +5,16 @@ USE ieee.std_logic_1164.ALL;
 ENTITY PC_REGISTER IS
     GENERIC (N : INTEGER := 32);
     PORT (
-        D                : IN STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
-        CLK, RST, HLT : IN STD_LOGIC;
-        Q                : OUT STD_LOGIC_VECTOR (n - 1 DOWNTO 0)
+        D        : IN STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
+        CLK, HLT : IN STD_LOGIC;
+        Q        : OUT STD_LOGIC_VECTOR (n - 1 DOWNTO 0)
     );
 END ENTITY;
 ARCHITECTURE StructPC_REGISTER OF PC_REGISTER IS
 BEGIN
-    PROCESS (CLK, RST)
+    PROCESS (CLK)
     BEGIN
-        IF (RST = '1') THEN
-            Q <= (OTHERS => '0');
-        ELSIF falling_edge(CLK) AND HLT = '0' THEN
+        IF falling_edge(CLK) AND HLT = '0' THEN
             Q <= D;
         END IF;
     END PROCESS;
